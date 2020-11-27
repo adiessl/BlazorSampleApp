859d5715fc935c6eab38b2657ceeca63c76e9723
Alexander Diessl <42088160+adiessl@users.noreply.github.com>
2020-11-27 12:16:54 +0100

    Add simple logout page

---
+++ BlazorSampleApp/Pages/Logout.cshtml.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorSampleApp.Pages
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
+            await HttpContext.SignOutAsync();
+
+            return Redirect("login");
+        }
+    }
+}
